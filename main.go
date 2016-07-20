ef516a9a6a0fe577011155df44f9da346cc9e744
Bhavik Kumar <bhavikk85@gmail.com>
2016-07-20 20:13:46 +1200

    Add simple post handler

--- main.go
+++ main.go
@@ -5,16 +5,48 @@ import (
   "github.com/husobee/vestigo"
 )
 
+var names []string
+
 func main() {
   router := vestigo.NewRouter()
 
-  router.Get("/hello/:name", GetWelcomeHandler)
+  router.Get("/:name", GetNameHandler)
+  router.Post("/:name", PostNameHandler)
 
   http.ListenAndServe(":8080", router)
 }
 
-func GetWelcomeHandler(w http.ResponseWriter, r *http.Request)  {
+func GetNameHandler(w http.ResponseWriter, r *http.Request)  {
   name := vestigo.Param(r, "name")
-  w.WriteHeader(200)
-  w.Write([]byte("Hello " + name))
+
+  if(NameExists(name)) {
+    w.WriteHeader(200)
+    w.Write([]byte(name))
+  } else {
+    w.WriteHeader(404)
+  }
+}
+
+func PostNameHandler(w http.ResponseWriter, r *http.Request)  {
+  name := vestigo.Param(r, "name")
+
+  exists := NameExists(name)
+  if(!exists) {
+    names = append(names, name)
+    w.Header().Add("Location","/" + name)
+    w.WriteHeader(201)
+  } else {
+    w.WriteHeader(200)
+  }
+  w.Write([]byte(name))
+
+}
+
+func NameExists(name string) bool {
+  for _, n := range names {
+    if(n == name) {
+      return true
+    }
+  }
+  return false
 }
