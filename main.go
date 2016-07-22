149f9d1eb08565234304c5dc914f24496976efb8
Bhavik Kumar <bhavikk85@gmail.com>
2016-07-23 10:53:29 +1200

    Add tests for handlers and simple User type

--- main.go
+++ main.go
@@ -3,23 +3,32 @@ package main
 import (
   "net/http"
   "github.com/husobee/vestigo"
+  "encoding/json"
+  "io/ioutil"
 )
 
-var names []string
+type User struct {
+  Name string `json:"name"`
+}
+
+var users []User
 
 func main() {
+  http.ListenAndServe(":8080", CreateRouter())
+}
+
+func CreateRouter() *vestigo.Router {
   router := vestigo.NewRouter()
 
   router.Get("/:name", GetNameHandler)
-  router.Post("/:name", PostNameHandler)
-
-  http.ListenAndServe(":8080", router)
+  router.Post("/", PostNameHandler)
+  return router
 }
 
 func GetNameHandler(w http.ResponseWriter, r *http.Request)  {
   name := vestigo.Param(r, "name")
 
-  if(NameExists(name)) {
+  if UserExists(name) {
     w.WriteHeader(200)
     w.Write([]byte(name))
   } else {
@@ -28,23 +37,32 @@ func GetNameHandler(w http.ResponseWriter, r *http.Request)  {
 }
 
 func PostNameHandler(w http.ResponseWriter, r *http.Request)  {
-  name := vestigo.Param(r, "name")
-
-  exists := NameExists(name)
-  if(!exists) {
-    names = append(names, name)
-    w.Header().Add("Location","/" + name)
-    w.WriteHeader(201)
-  } else {
-    w.WriteHeader(200)
+  var user User
+  body, _ := ioutil.ReadAll(r.Body)
+  err := json.Unmarshal(body, &user)
+  if err != nil {
+      w.WriteHeader(http.StatusBadRequest)
+      return
   }
-  w.Write([]byte(name))
 
+  w.Header().Add("Content-Type", "application/json; charset=UTF-8")
+  w.Header().Add("Location","/" + user.Name)
+  if !UserExists(user.Name) {
+    AppendUser(user)
+    w.WriteHeader(http.StatusCreated)
+  } else {
+    w.WriteHeader(http.StatusOK)
+  }
+  json.NewEncoder(w).Encode(user)
 }
 
-func NameExists(name string) bool {
-  for _, n := range names {
-    if(n == name) {
+func AppendUser(user User) {
+  users = append(users, user)
+}
+
+func UserExists(name string) bool {
+  for _, u := range users {
+    if(u.Name == name) {
       return true
     }
   }
