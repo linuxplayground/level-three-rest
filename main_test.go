ab43a4a553a7ce33a40a4f3f01d743d48ed7ba65
Bhavik Kumar <bhavikk85@gmail.com>
2016-08-26 21:29:03 +1200

    Remove tests for main

--- main_test.go
+++ main_test.go
@@ -1,79 +1,79 @@
 package main
 
-import (
-  "net/http"
-  "net/http/httptest"
-	"testing"
-  "github.com/husobee/vestigo"
-  "github.com/stretchr/testify/assert"
-  "encoding/json"
-  "bytes"
-)
-
-var (
-  router *vestigo.Router
-)
-
-func init() {
-  router = CreateRouter()
-}
-
-func TestUserDoesNotExists(t *testing.T) {
-  exists := UserExists("does-not-exist")
-  assert.False(t, exists)
-}
-
-func TestAppendUser(t *testing.T) {
-  user := User{Name:"test-user"}
-  AppendUser(user)
-  exists := UserExists(user.Name)
-  assert.True(t, exists)
-}
-
-func TestPostNameHandlerNewUser(t *testing.T) {
-  user := User{Name:"test-user-2"}
-  u, _ := json.Marshal(user)
-  req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(u))
-  w := httptest.NewRecorder()
-  router.ServeHTTP(w, req)
-  assert.Equal(t, http.StatusCreated, w.Code)
-}
-
-func TestPostNameHandlerAlreadyExists(t *testing.T) {
-  user := User{Name:"test-user-2"}
-  u, _ := json.Marshal(user)
-  req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(u))
-  w := httptest.NewRecorder()
-  router.ServeHTTP(w, req)
-  assert.Equal(t, http.StatusOK, w.Code)
-}
-
-func TestPostNameHandlerInvalidJson(t *testing.T) {
-  user := `{ foo": "bar" }`
-  req, _ := http.NewRequest("POST", "/", bytes.NewBuffer([]byte(user)))
-  w := httptest.NewRecorder()
-  router.ServeHTTP(w, req)
-  assert.Equal(t, http.StatusBadRequest, w.Code)
-}
-
-func TestGetNameHandler(t *testing.T) {
-  user := User{Name:"test-user-3"}
-  u, _ := json.Marshal(user)
-  req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(u))
-  w := httptest.NewRecorder()
-  router.ServeHTTP(w, req)
-
-  gReq, _ := http.NewRequest("GET", "/" + user.Name, nil)
-  gW := httptest.NewRecorder()
-  router.ServeHTTP(gW, gReq)
-
-  assert.Equal(t, http.StatusOK, gW.Code)
-}
-
-func TestGetNameHandlerDoesNotExist(t *testing.T) {
-  gReq, _ := http.NewRequest("GET", "/foo", nil)
-  gW := httptest.NewRecorder()
-  router.ServeHTTP(gW, gReq)
-
-  assert.Equal(t, http.StatusNotFound, gW.Code)
-}
+// import (
+//   "net/http"
+//   "net/http/httptest"
+// 	"testing"
+//   "github.com/husobee/vestigo"
+//   "github.com/stretchr/testify/assert"
+//   "encoding/json"
+//   "bytes"
+// )
+//
+// var (
+//   router *vestigo.Router
+// )
+//
+// func init() {
+//   router = CreateRouter()
+// }
+//
+// func TestUserDoesNotExists(t *testing.T) {
+//   exists := UserExists("does-not-exist")
+//   assert.False(t, exists)
+// }
+//
+// func TestAppendUser(t *testing.T) {
+//   user := User{Name:"test-user"}
+//   AppendUser(user)
+//   exists := UserExists(user.Name)
+//   assert.True(t, exists)
+// }
+//
+// func TestPostNameHandlerNewUser(t *testing.T) {
+//   user := User{Name:"test-user-2"}
+//   u, _ := json.Marshal(user)
+//   req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(u))
+//   w := httptest.NewRecorder()
+//   router.ServeHTTP(w, req)
+//   assert.Equal(t, http.StatusCreated, w.Code)
+// }
+//
+// func TestPostNameHandlerAlreadyExists(t *testing.T) {
+//   user := User{Name:"test-user-2"}
+//   u, _ := json.Marshal(user)
+//   req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(u))
+//   w := httptest.NewRecorder()
+//   router.ServeHTTP(w, req)
+//   assert.Equal(t, http.StatusOK, w.Code)
+// }
+//
+// func TestPostNameHandlerInvalidJson(t *testing.T) {
+//   user := `{ foo": "bar" }`
+//   req, _ := http.NewRequest("POST", "/", bytes.NewBuffer([]byte(user)))
+//   w := httptest.NewRecorder()
+//   router.ServeHTTP(w, req)
+//   assert.Equal(t, http.StatusBadRequest, w.Code)
+// }
+//
+// func TestGetNameHandler(t *testing.T) {
+//   user := User{Name:"test-user-3"}
+//   u, _ := json.Marshal(user)
+//   req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(u))
+//   w := httptest.NewRecorder()
+//   router.ServeHTTP(w, req)
+//
+//   gReq, _ := http.NewRequest("GET", "/" + user.Name, nil)
+//   gW := httptest.NewRecorder()
+//   router.ServeHTTP(gW, gReq)
+//
+//   assert.Equal(t, http.StatusOK, gW.Code)
+// }
+//
+// func TestGetNameHandlerDoesNotExist(t *testing.T) {
+//   gReq, _ := http.NewRequest("GET", "/foo", nil)
+//   gW := httptest.NewRecorder()
+//   router.ServeHTTP(gW, gReq)
+//
+//   assert.Equal(t, http.StatusNotFound, gW.Code)
+// }
